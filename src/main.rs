3720779d62a8ba857c5de724987d0122705df11c
Jacob Hughes <j@jacobhughes.me>
2020-01-23 06:47:43 -0500

    H Y P E R C A L L S

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::thread;
-use std::sync::{Arc,Barrier};
+use std::sync::{Arc,Barrier,Mutex};
 use std::fs::File;
 use std::io::Read;
 
 use goblin::elf::Elf;
 use goblin::elf::program_header::PT_LOAD;
+
+use crossbeam::channel;
 
 use kvm_ioctls::{Kvm, VmFd, VcpuFd, VcpuExit};
 use memmap::{MmapOptions,MmapMut};
@@ -14,8 +16,8 @@ use x86::bits64::paging::*;
 
 fn main() {
 
-    let mem_size = 0x40000;
-    let mut mem = MemAlloc::new(mem_size);
+    let mem_size = 0x400000;
+    let mem = MemAlloc::new(mem_size);
 
     let mut file1 = vec!();
     File::open("core1").unwrap().read_to_end(&mut file1).unwrap();
@@ -142,7 +144,7 @@ impl MemAlloc {
         }
 
         //let (pml4, _pt_pages) = self.create_map(&pages, vaddr);
-        let mut pt_pages = self.map_multiple(pml4_address, &pages, vaddr);
+        let _pt_pages = self.map_multiple(pml4_address, &pages, vaddr);
 
         self.identity_map(pml4_address, 0xFFFF800000000000);
         //for i in 0..(self.backing.len() / 4096) {
@@ -447,11 +449,11 @@ impl Exclusive {
     }
 }
 
-impl std::fmt::Debug for Exclusive {
-    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
-        Ok(())
-    }
-}
+//impl std::fmt::Debug for Exclusive {
+//    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+//        Ok(())
+//    }
+//}
 
 struct Program {
     vm: VmFd,
@@ -459,13 +461,15 @@ struct Program {
     entry: u64,
     cr3: u64,
     stack: u64,
-    exclusive: Box<Exclusive>
+    exclusive: Mutex<Box<Exclusive>>,
+    exclusive_addr: u64,
 }
 
 impl Program {
     /// Make a new VM using the given memory
     pub fn new(kvm: &Kvm, mem: &mut MmapMut, entry: u64, cr3: u64, stack: u64) -> Self {
         let vm = kvm.create_vm().unwrap();
+
 
         let mem_region = kvm_bindings::kvm_userspace_memory_region {
             slot: 0,
@@ -478,12 +482,14 @@ impl Program {
 
         let mut exclusive = Box::from(Exclusive::new());
 
+        let exclusive_addr = mem.len() as u64;
+
         let exclusive_region = kvm_bindings::kvm_userspace_memory_region {
             slot: 1,
-            guest_phys_addr: mem.len() as u64,
+            guest_phys_addr: exclusive_addr,
             memory_size: 4096,
-            userspace_addr: (&mut exclusive.0[0] as *mut u8) as u64,
-            flags: 0
+            userspace_addr: exclusive.0.as_mut_ptr() as u64,
+            flags: 0,
         };
         unsafe { vm.set_user_memory_region(exclusive_region).unwrap() };
 
@@ -493,7 +499,8 @@ impl Program {
             entry,
             cr3,
             stack,
-            exclusive
+            exclusive: Mutex::new(exclusive),
+            exclusive_addr
         }
     }
 
@@ -501,17 +508,94 @@ impl Program {
         self.cores.len() as u8
     }
 
+    fn with_exclusive<T,F: FnMut(&mut Exclusive) -> T>(&self, mut f: F) ->T {
+        let mut exclusive = self.exclusive.lock().unwrap();
+
+        let exclusive_region_immut = kvm_bindings::kvm_userspace_memory_region {
+            slot: 1,
+            guest_phys_addr: self.exclusive_addr,
+            memory_size: 0,
+            userspace_addr: exclusive.0.as_ptr() as u64,
+            flags: 0
+            //flags: kvm_bindings::KVM_MEM_READONLY // doesn't work on my machine
+        };
+
+        let res = unsafe { self.vm.set_user_memory_region(exclusive_region_immut) };
+        res.map_err(|e| std::io::Error::from_raw_os_error(e.errno())).unwrap();
+
+        let ret = f(&mut exclusive);
+
+        let exclusive_region_mut = kvm_bindings::kvm_userspace_memory_region {
+            slot: 1,
+            guest_phys_addr: self.exclusive_addr,
+            memory_size: 4096,
+            userspace_addr: exclusive.0.as_mut_ptr() as u64,
+            flags: 0
+        };
+
+        unsafe { self.vm.set_user_memory_region(exclusive_region_mut).unwrap() };
+
+        ret
+    }
+
     /// Create a new core for our VM
     pub fn new_core(&mut self) -> u8 {
         let id = self.cores.len() as u8;
         let c = VMCore::new(self.vm.create_vcpu(id).unwrap());
-        c.load_registers(self.entry, self.cr3, self.stack);
+        c.load_registers(self.entry, self.cr3, self.stack, self.exclusive_addr);
         self.cores.push(c);
         id
     }
 
-    pub fn run_core(&self, core_id: u8) -> Result<VcpuExit, ()> {
-        self.cores[core_id as usize].run()
+    pub fn run_core(&self, core_id: u8, chan: channel::Sender<HyperMessage>) -> Result<(), ()> {
+        loop {
+            match self.cores[core_id as usize].run()? {
+                VcpuExit::Hlt => {
+                    let mut regs = self.cores[core_id as usize].vcpu.get_regs().unwrap();
+                    println!("HALT: {:?}", regs.rax);
+                    regs.rax = match regs.rax {
+                        // Print Text
+                        1 => {
+                            let mem = self.with_exclusive(|e| {
+                                e.0.clone()
+                            });
+                            let offset = regs.rdi as usize;
+                            let len = regs.rsi as usize;
+                            if offset < mem.len() && (offset + len) < mem.len() {
+                                if let Ok(s) = std::str::from_utf8(&mem[offset..(offset + len)]) {
+                                    chan.send(HyperMessage::Print(s.to_owned()));
+                                    0
+                                } else {
+                                    std::u64::MAX
+                                }
+                            } else {
+                                std::u64::MAX
+                            }
+                        }
+                        // Submit value
+                        2 => {
+                            self.with_exclusive(|e| {
+                                e.0
+                            });
+                            0
+                        }
+                        _ => {
+                            std::u64::MAX
+                        }
+                    };
+                    self.cores[core_id as usize].vcpu.set_regs(&regs).unwrap();
+                }
+                VcpuExit::Shutdown => {
+                    let regs = self.cores[core_id as usize].vcpu.get_regs().unwrap();
+                    println!("Shutting down {:?}", regs);
+                    break
+                }
+                e => {
+                    panic!("unexpected vcpu exit {:?}", e);
+                }
+            }
+        }
+        Ok(())
     }
 }
 
@@ -523,9 +607,11 @@ struct Arena {
 
 impl Arena {
     pub fn new(mem: MemAlloc) -> Self {
+        let kvm = Kvm::new().unwrap();
+        println!("readonlymem: {:?}", kvm.check_extension(kvm_ioctls::Cap::ReadonlyMem));
         Self {
             mem,
-            kvm: Kvm::new().unwrap(),
+            kvm,
             programs: vec![],
         }
     }
@@ -544,15 +630,21 @@ impl Arena {
             if h.p_type != PT_LOAD {
                 continue
             }
-            let code = &executable[h.p_offset as usize..(h.p_offset as usize + h.p_filesz as usize)];
-            self.mem.load2(cr3 as usize, code, h.p_vaddr as usize);
+            let mut code = vec![0; h.p_memsz as usize];
+            if h.p_filesz > 0 {
+                code.copy_from_slice(&executable[h.p_offset as usize..(h.p_offset as usize + h.p_filesz as usize)]);
+            }
+            self.mem.load2(cr3 as usize, &code, h.p_vaddr as usize);
         }
         let entry = elf.header.e_entry;
-        let stack = 0x00007FFFFFFFEFFF;
-        self.mem.lookup_or_allocate(cr3 as usize, (stack - (stack % 4096)) as usize);
-        self.mem.lookup_or_allocate(cr3 as usize, (stack - (stack % 4096) - 4096) as usize);
+        let stack = 0x00007FFFFFFFDFFF;
+        for i in 0..10 {
+            self.mem.lookup_or_allocate(cr3 as usize, (stack - (stack % 4096) - (4096 * i)) as usize);
+        }
         println!("e: {:x}", entry);
         self.mem.identity_map(cr3 as usize, 0xFFFF800000000000);
+
+        self.mem.map(cr3 as usize, self.mem.backing.len() as u64, 0x00007FFFFFFFE000);
 
         let mut prog = Program::new(&self.kvm, &mut self.mem.backing, entry, cr3, stack);
         prog.new_core();
@@ -560,31 +652,13 @@ impl Arena {
     }
 
     /// Run all programs
-    pub fn run(&mut self) {
+    pub fn run(mut self) {
         //let core1 = vm.new_core();
         //let core2 = vm.new_core();
+        thread::spawn(move || {
+            self.run_threads()
+        }).join();
 
-        let num_cores = self.programs.iter().map(|p|p.core_count() as usize).sum();
-        let barrier = Arc::new(Barrier::new(num_cores));
-        crossbeam::scope(|scope| {
-            self.programs.iter().flat_map( |p|{
-                let b2 = barrier.clone();
-                println!("{:?}", p.cores.len());
-                        println!("help me");
-                (0..p.core_count()).map( move |i|{
-                    let b = b2.clone();
-                    let i = i;
-                    scope.spawn(move |_|{
-                        println!("help 1");
-                        b.wait();
-                        println!("help 2");
-                        println!("{:?}", p.run_core(i));
-                    })
-                })
-            }).for_each(|h| {
-                
-            });
-        });
         //let b1 = barrier.clone();
         //let h1 = thread::spawn(move ||{
         //    barrier.wait();
@@ -598,6 +672,47 @@ impl Arena {
         //h2.join().unwrap();
         ////run64bit(mem.consume(), 0x1000, cr3);
     }
+    fn run_threads(&mut self) {
+
+        let num_cores = self.programs.iter().map(|p|p.core_count() as usize).sum();
+
+        let (send, recv) = channel::bounded::<HyperMessage>(num_cores);
+
+        let barrier = Arc::new(Barrier::new(num_cores));
+        crossbeam::scope(move |scope| {
+            self.programs.iter().flat_map(|p| {
+                (0..p.core_count()).map( move |i|{
+                    (p, i)
+                })
+            }).for_each(|(p, i)| {
+                use std::os::unix::io::AsRawFd;
+                println!("{:?}: {:?}", p.vm.as_raw_fd(), i);
+                let s = send.clone();
+                let b = barrier.clone();
+                scope.spawn(move |_|{
+                    println!("help 1");
+                    b.wait();
+                    println!("help 2");
+                    println!("{:?}", p.run_core(i, s));
+                });
+            });
+            loop {
+                match recv.recv() {
+                    Ok(HyperMessage::Print(s)) => {
+                        println!("message: {:?}", s);
+                    }
+                    Err(e) => {
+                        println!("{:?}", e);
+                        break
+                    }
+                }
+            }
+        }).unwrap();
+    }
+}
+
+enum HyperMessage {
+    Print(String)
 }
 
 /// One core of our virtual machine
@@ -633,7 +748,7 @@ impl VMCore {
         }
     }
 
-    pub fn load_registers(&self, entry: u64, cr3: u64, stack: u64) {
+    pub fn load_registers(&self, entry: u64, cr3: u64, stack: u64, mem_len: u64) {
         let mut vcpu_sregs = self.vcpu.get_sregs().unwrap();
         vcpu_sregs.cr3 = cr3;
         self.vcpu.set_sregs(&vcpu_sregs).unwrap();
@@ -641,12 +756,13 @@ impl VMCore {
         let mut vcpu_regs = self.vcpu.get_regs().unwrap();
         vcpu_regs.rip = entry;
         vcpu_regs.rsp = stack;
+        vcpu_regs.rdi = mem_len;
         self.vcpu.set_regs(&vcpu_regs).unwrap();
     }
 
     /// Run some 64 bit mode idgaf
     pub fn run64bit(&self, entry: u64, cr3: u64, stack: u64) {
-        self.load_registers(entry, cr3, stack);
+        self.load_registers(entry, cr3, stack, 0);
 
         loop {
             match self.run().expect("run failed") {
